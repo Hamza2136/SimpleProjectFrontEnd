04a84e8971e9d9830d1e459ad5fddefda0ca1f73
Hamza Abid <hamzaabid208@gmail.com>
2024-09-01 11:41:47 +0500

    Added enviroment for apiUrl

--- src/app/Services/data.service.ts
+++ src/app/Services/data.service.ts
@@ -3,7 +3,8 @@ import { Injectable } from '@angular/core';
 import { DataModel } from '../Models/DataModel';
 import { Observable } from 'rxjs';
 import { ApiResponse } from '../Models/ApiResponse';
-import { LoginModel } from '../Models/LoginModel';
+import { environment } from '../../environments/environment.development';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,33 +13,33 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
   createData(obj: DataModel): Observable<ApiResponse>{
-    return this.http.post<ApiResponse>("https://localhost:7132/api/Main/CreateData", obj);
+    return this.http.post<ApiResponse>(environment.LOCAL_API_URL+"api/Main/CreateData", obj);
   }
 
   getAllData():Observable<DataModel[]>{
-    return this.http.get<DataModel[]>("https://localhost:7132/api/Main/GetAll");
+    return this.http.get<DataModel[]>(environment.LOCAL_API_URL+"api/Main/GetAll");
   }
 
   getData():Observable<ApiResponse>{
-    return this.http.get<ApiResponse>("https://localhost:7132/api/Main/GetAll");
+    return this.http.get<ApiResponse>(environment.LOCAL_API_URL+"api/Main/GetAll");
   }
 
   getDataBySerialNumber(serialNumber:number): Observable<ApiResponse>{
-    return this.http.get<ApiResponse>("https://localhost:7132/api/Main/GetBySerialNumber?serialNumber="+serialNumber);
+    return this.http.get<ApiResponse>(environment.LOCAL_API_URL+"api/Main/GetBySerialNumber?serialNumber="+serialNumber);
   }
 
   deleteData(serialNumber: number):Observable<ApiResponse>{
-    return this.http.delete<ApiResponse>("https://localhost:7132/api/Main/DeleteData?serialNumber="+serialNumber);
+    return this.http.delete<ApiResponse>(environment.LOCAL_API_URL+"api/Main/DeleteData?serialNumber="+serialNumber);
   }
 
   //Extra if needed
   updateData(serialNumber:number, obj:DataModel): Observable<ApiResponse>{
-    return this.http.put<ApiResponse>("https://localhost:7132/api/Main/UpdateData?serialNumber="+serialNumber, obj);
+    return this.http.put<ApiResponse>(environment.LOCAL_API_URL+"api/Main/UpdateData?serialNumber="+serialNumber, obj);
   }
   
   // login service
   login(email:string, password:string): Observable<ApiResponse> {
-    return this.http.get<ApiResponse>(`https://localhost:7132/api/Main/Login?emailId=${email}&password=${password}`);
+    return this.http.get<ApiResponse>(environment.LOCAL_API_URL+`api/Main/Login?emailId=${email}&password=${password}`);
   }
 
   //Upload Zip File Link
@@ -49,6 +50,6 @@ export class DataService {
       formData.append('files', file, file.name);
     }
     formData.append('uploadLink', uploadLink);
-    return this.http.post<ApiResponse>("https://localhost:7132/api/Main/UploadFiles?uploadLink="+uploadLink, formData);
+    return this.http.post<ApiResponse>(environment.LOCAL_API_URL+"api/Main/UploadFiles?uploadLink="+uploadLink, formData);
   }
 }
